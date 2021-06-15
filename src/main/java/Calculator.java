8096c0e7b24781ccf07edfad46a0a7554ca1afbc
Vladimir <mkkinj@gmail.com>
2021-06-15 23:55:38 +0300

    init project

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+
+    public  int sum(int a, int b){
+        return a+b;
+    }
+
+    public  int div(int a, int b){
+        return a/b;
+    }
+
+    public double divDouble(double a, double b){
+        return a/b;
+    }
+
+    public double sqr_t(double a){
+        return Math.sqrt(a);
+    }
+
+}
---
+++ src/test/java/Unittest.java
@@ -0,0 +1,100 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Unittest {
+    // a>0, b>0
+    @Test
+    public void positiveSumTest1() {
+        Calculator calc = new Calculator();
+
+        Assert.assertEquals(calc.sum(2, 3), 5, "Тестовый комментарий...");
+    }
+
+    // a=0, b=0
+    @Test
+    public void positiveSumTest2() {
+        Calculator calc = new Calculator();
+
+        Assert.assertEquals(calc.sum(0, 0), 0);
+    }
+
+    // a<0, b>0
+    @Test
+    public void positiveSumTest3() {
+        Calculator calc = new Calculator();
+
+        Assert.assertEquals(calc.sum(-2, 3), 1);
+    }
+
+    // a<0, b<0
+    @Test
+    public void positiveSumTest4() {
+        Calculator calc = new Calculator();
+
+        Assert.assertEquals(calc.sum(-2, -3), -5);
+    }
+
+    @Test
+    public void positiveDivTest1(){
+        Calculator calculator = new Calculator();
+
+        Assert.assertEquals(calculator.div(10,5), 2);
+    }
+
+    @Test
+    public void negativeDivTest2(){
+        final Calculator calculator = new Calculator();
+
+        Assert.assertThrows(java.lang.ArithmeticException.class, ()-> calculator.div(-2, 0));
+
+    }
+
+    // a>0,b<0
+    @Test
+    public void positiveDivDoubleTest1(){
+        Calculator calculator = new Calculator();
+
+        Assert.assertEquals(calculator.divDouble(4, 2.5), 1.6);
+    }
+
+    // a<0, b<0
+    @Test
+    public void positiveDivDoubleTest2(){
+        Calculator calculator = new Calculator();
+
+        Assert.assertEquals(calculator.divDouble(-4, -2.5), 1.6);
+    }
+
+    // a<0, b>0
+    @Test
+    public void positiveDivDoubleTest3(){
+        Calculator calculator = new Calculator();
+
+        Assert.assertEquals(calculator.divDouble(-4, 2.5), -1.6);
+    }
+
+    // a>0, b<0
+    @Test
+    public void positiveDivDoubleTest4(){
+        Calculator calculator = new Calculator();
+
+        Assert.assertEquals(calculator.divDouble(4, -2.5), 1.6);
+    }
+
+    // a=0, b>0
+    @Test
+    public void positiveDivDoubleTest5(){
+        Calculator calculator = new Calculator();
+
+        Assert.assertEquals(calculator.divDouble(0, 2.5), 0);
+    }
+
+    // a>0, b=0
+    @Test
+    public void negativeDivDoubleTest(){
+        Calculator calculator = new Calculator();
+
+        //Assert.assertEquals(calculator.divDouble(4, 0), 0);
+        Assert.assertNotEquals(calculator.divDouble(4, 0), 0,0);
+    }
+}

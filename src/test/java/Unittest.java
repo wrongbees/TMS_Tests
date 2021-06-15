9ac3200b9bdbb0ef1e18753c407af4ddd3636b64
Vladimir <mkkinj@gmail.com>
2021-06-16 01:14:18 +0300

    Lesson 3 готов ещё раз

--- src/test/java/Unittest.java
+++ src/test/java/Unittest.java
@@ -3,51 +3,51 @@ import org.testng.annotations.Test;
 
 public class Unittest {
     // a>0, b>0
-    @Test
-    public void positiveSumTest1() {
-        Calculator calc = new Calculator();
-
-        Assert.assertEquals(calc.sum(2, 3), 5, "Тестовый комментарий...");
-    }
-
-    // a=0, b=0
-    @Test
-    public void positiveSumTest2() {
-        Calculator calc = new Calculator();
-
-        Assert.assertEquals(calc.sum(0, 0), 0);
-    }
-
-    // a<0, b>0
-    @Test
-    public void positiveSumTest3() {
-        Calculator calc = new Calculator();
-
-        Assert.assertEquals(calc.sum(-2, 3), 1);
-    }
-
-    // a<0, b<0
-    @Test
-    public void positiveSumTest4() {
-        Calculator calc = new Calculator();
-
-        Assert.assertEquals(calc.sum(-2, -3), -5);
-    }
-
-    @Test
-    public void positiveDivTest1(){
-        Calculator calculator = new Calculator();
-
-        Assert.assertEquals(calculator.div(10,5), 2);
-    }
-
-    @Test
-    public void negativeDivTest2(){
-        final Calculator calculator = new Calculator();
-
-        Assert.assertThrows(java.lang.ArithmeticException.class, ()-> calculator.div(-2, 0));
-
-    }
+//    @Test
+//    public void positiveSumTest1() {
+//        Calculator calc = new Calculator();
+//
+//        Assert.assertEquals(calc.sum(2, 3), 5, "Тестовый комментарий...");
+//    }
+//
+//    // a=0, b=0
+//    @Test
+//    public void positiveSumTest2() {
+//        Calculator calc = new Calculator();
+//
+//        Assert.assertEquals(calc.sum(0, 0), 0);
+//    }
+//
+//    // a<0, b>0
+//    @Test
+//    public void positiveSumTest3() {
+//        Calculator calc = new Calculator();
+//
+//        Assert.assertEquals(calc.sum(-2, 3), 1);
+//    }
+//
+//    // a<0, b<0
+//    @Test
+//    public void positiveSumTest4() {
+//        Calculator calc = new Calculator();
+//
+//        Assert.assertEquals(calc.sum(-2, -3), -5);
+//    }
+//
+//    @Test
+//    public void positiveDivTest1(){
+//        Calculator calculator = new Calculator();
+//
+//        Assert.assertEquals(calculator.div(10,5), 2);
+//    }
+//
+//    @Test
+//    public void negativeDivTest2(){
+//        final Calculator calculator = new Calculator();
+//
+//        Assert.assertThrows(java.lang.ArithmeticException.class, ()-> calculator.div(-2, 0));
+//
+//    }
 
     // a>0,b<0
     @Test
@@ -78,7 +78,7 @@ public class Unittest {
     public void positiveDivDoubleTest4(){
         Calculator calculator = new Calculator();
 
-        Assert.assertEquals(calculator.divDouble(4, -2.5), 1.6);
+        Assert.assertEquals(calculator.divDouble(4, -2.5), -1.6);
     }
 
     // a=0, b>0
@@ -91,10 +91,35 @@ public class Unittest {
 
     // a>0, b=0
     @Test
-    public void negativeDivDoubleTest(){
+    public void negativeDivDoubleTest(){                       //можно ли этот тест считать негативным?????
         Calculator calculator = new Calculator();
 
-        //Assert.assertEquals(calculator.divDouble(4, 0), 0);
-        Assert.assertNotEquals(calculator.divDouble(4, 0), 0,0);
+        Assert.assertEquals(calculator.divDouble(4, 0), Double.POSITIVE_INFINITY);
+
     }
+
+    // a>0
+    @Test
+    public void positiveSqrlTest1(){
+        Calculator calculator = new Calculator();
+
+        Assert.assertEquals(calculator.sqr_t(4), 2);
+    }
+
+    // a=0
+    @Test
+    public void positiveSqrlTest2(){
+        Calculator calculator = new Calculator();
+
+        Assert.assertEquals(calculator.sqr_t(0), 0);
+    }
+
+    // a<0
+    @Test
+    public void negativeSqrlTest(){               //можно ли этот тест считать негативным?????
+        Calculator calculator = new Calculator();
+
+        Assert.assertEquals(Double.isNaN(calculator.sqr_t(-4)), true);
+    }
+
 }
